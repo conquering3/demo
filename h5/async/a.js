f770d427603b235e0bb4feb271d7562aee6e0531
conquering <15817187513@163.com>
2018-04-25 14:15:34 +0800

    180425

---
+++ h5/async/a.js
@@ -0,0 +1,5 @@
+let a = [];
+        for (let i = 0; i < 1000; i++) {
+            a.push(i);
+        }
+        console.log('a:', a);
---
+++ h5/async/b.js
@@ -0,0 +1,5 @@
+let b = [];
+        for (let i = 0; i < 1; i++) {
+            b.push(i);
+        }
+        console.log('b:', b);
